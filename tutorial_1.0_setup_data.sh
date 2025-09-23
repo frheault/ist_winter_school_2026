f93cefe156f6caf0ed9ca189c1999600af41fd7c
frheault <francois.m.rheault@usherbrooke.ca>
2025-09-23 09:28:11 -0400

    Fix dicom files

--- tutorial_1.0_setup_data.sh
+++ tutorial_1.0_setup_data.sh
@@ -16,14 +16,14 @@
 
 # Step 1: Create the BIDS Directory Structure
 
-unzip dicom_filtered_sub01.zip -d data/
+unzip dicom_filtered_sub01.zip
 mkdir nifti_data
-dcm2niix -o nifti_data/ -z y dicom_data/12_09_2023_MT_orientation_sub01/
+dcm2niix -o nifti_data/ -z y dicom_data/
 
 echo "Step 1: Creating BIDS directory structure..."
 mkdir -p bids_data/sub-01/ses-01/dwi
 mkdir -p bids_data/sub-01/ses-01/anat
-mrconvert nifti_data/12_09_2023_MT_orientation_sub01_WIP_DWI_20230912152935_1201.nii.gz bids_data/sub-01/ses-01/dwi/dwi.nii.gz -stride 1,2,3,4
-cp nifti_data/12_09_2023_MT_orientation_sub01_WIP_DWI_20230912152935_1201.bval bids_data/sub-01/ses-01/dwi/dwi.bval
-cp nifti_data/12_09_2023_MT_orientation_sub01_WIP_DWI_20230912152935_1201.bvec bids_data/sub-01/ses-01/dwi/dwi.bvec
-cp nifti_data/12_09_2023_MT_orientation_sub01_WIP_3D_T1_20230912152935_201.nii.gz bids_data/sub-01/ses-01/anat/t1.nii.gz -stride 1,2,3,4
+mrconvert nifti_data/dicom_data_WIP_DWI_20230912152935_1201.nii.gz bids_data/sub-01/ses-01/dwi/dwi.nii.gz -stride 1,2,3,4
+cp nifti_data/dicom_data_WIP_DWI_20230912152935_1201.bval bids_data/sub-01/ses-01/dwi/dwi.bval
+cp nifti_data/dicom_data_WIP_DWI_20230912152935_1201.bvec bids_data/sub-01/ses-01/dwi/dwi.bvec
+cp nifti_data/dicom_data_WIP_3D_T1_20230912152935_901.nii.gz bids_data/sub-01/ses-01/anat/t1.nii.gz -stride 1,2,3,4
